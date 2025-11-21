17ea708f43b25d68cbe2073bdab411a39fe5f88c
Abdul Aziz Mubarak <abdulazizmubarak1@gmail.com>
2025-11-21 13:25:07 +0800

    Create visual-branding.blade.php

---
+++ web-tcg/resources/views/frontend/contents/services/visual-branding.blade.php
@@ -0,0 +1,11 @@
+@extends('frontend.layouts.app')
+@section('title')
+    Tentang Kami | Tribun Consulting Group
+@endsection
+
+@section('meta')
+@endsection
+
+@section('content')
+<h2>Digital Visual dan Digital Branding</h2>
+@endsection
