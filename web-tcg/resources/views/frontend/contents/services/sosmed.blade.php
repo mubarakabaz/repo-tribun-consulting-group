c1147ba1302f168ab10ddd5715e370dc578f62b0
Abdul Aziz Mubarak <abdulazizmubarak1@gmail.com>
2025-11-21 13:25:49 +0800

    Create sosmed.blade.php

---
+++ web-tcg/resources/views/frontend/contents/services/sosmed.blade.php
@@ -0,0 +1,11 @@
+@extends('frontend.layouts.app')
+@section('title')
+    Tentang Kami | Tribun Consulting Group
+@endsection
+
+@section('meta')
+@endsection
+
+@section('content')
+<h2>Sosmed</h2>
+@endsection
