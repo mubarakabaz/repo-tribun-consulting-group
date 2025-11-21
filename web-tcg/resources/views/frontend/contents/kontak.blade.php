f2bce359d38755e985aea4a60d35e3f59b425a4f
Abdul Aziz Mubarak <abdulazizmubarak1@gmail.com>
2025-11-21 13:24:18 +0800

    Create kontak.blade.php

---
+++ web-tcg/resources/views/frontend/contents/kontak.blade.php
@@ -0,0 +1,11 @@
+@extends('frontend.layouts.app')
+@section('title')
+    Tentang Kami | Tribun Consulting Group
+@endsection
+
+@section('meta')
+@endsection
+
+@section('content')
+<h2>Kontak</h2>
+@endsection
