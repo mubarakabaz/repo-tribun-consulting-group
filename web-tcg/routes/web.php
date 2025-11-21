32553ca6f1408dc720511bf82ae2caecab682021
Abdul Aziz Mubarak <abdulazizmubarak1@gmail.com>
2025-11-21 13:26:56 +0800

    Update web.php

--- web-tcg/routes/web.php
+++ web-tcg/routes/web.php
@@ -3,18 +3,20 @@
 use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
+// Route umum untuk Tamu
 Route::get('/', [FrontController::class, 'index'])->name('index');
 Route::get('about', [FrontController::class, 'about'])->name('about');
+Route::get('pelatihan', [FrontController::class, 'pelatihan'])->name('pelatihan');
+Route::get('artikel', [FrontController::class, 'artikel'])->name('artikel');
+Route::get('kontak', [FrontController::class, 'kontak'])->name('kontak');
+// Route Layanan
+Route::get('/layanan/seo', [FrontController::class, 'servicesSeo'])->name('seo');
+Route::get('/layanan/iklan-digital-marketing', [FrontController::class, 'servicesAds'])->name('ads');
+// Route::get('/layanan/manajemen-sosial-media', [FrontController::class, 'servicesSosmed'])->name('sosmed');
+Route::get('/layanan/jasa-pembuatan-website', [FrontController::class, 'servicesWeb'])->name('website');
+Route::get('/layanan/content-marketing', [FrontController::class, 'servicesContentMarketing'])->name('content-marketing');
+Route::get('/layanan/desain-visual-dan-digital-branding', [FrontController::class, 'servicesVisualBranding'])->name('visual-branding');
+
+
 
 Route::post('/subscribe', [FrontController::class, 'subscribeStore'])->name('subscribe');
